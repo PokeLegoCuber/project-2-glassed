5b62f48380ee7b3a391697c10a96882c463b5f65
kevinwong197 <kevinwong1997@hotmail.com>
2018-03-15 12:51:47 -0400

    added random treasure selection

--- TreasureHuntOnConsoleEnhanced/src/main/java/edu/cuny/brooklyn/project/treasure/TreasureGenerator.java
+++ TreasureHuntOnConsoleEnhanced/src/main/java/edu/cuny/brooklyn/project/treasure/TreasureGenerator.java
@@ -1,9 +1,19 @@
 package edu.cuny.brooklyn.project.treasure;
 
 import edu.cuny.brooklyn.project.GameSettings;
+import java.util.Random;
 
 public class TreasureGenerator {
 	public Treasure generate() {
-		return new SquareTreasure(GameSettings.DEFAULT_TREASURE_SIZE);
+		Random rand = new Random();
+		int randnum = rand.nextInt(2);		
+		switch(randnum) {
+		case 0:
+			return new SquareTreasure(GameSettings.DEFAULT_TREASURE_SIZE);
+		case 1:
+			return new RectangleTreasure(GameSettings.DEFAULT_TREASURE_SIZE);
+		default:
+			return new SquareTreasure(GameSettings.DEFAULT_TREASURE_SIZE);
+		}
 	}
 }
