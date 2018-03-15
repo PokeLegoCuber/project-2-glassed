21861d95a4e914ef86a13e4dd99ea439b90e4e17
Jefferson Phanor <35940979+Blazingwolferno@users.noreply.github.com>
2018-03-15 14:41:58 -0400

    Add files via upload
    i added circle and triangle treasure

---
+++ TreasureHuntOnConsoleEnhanced/src/main/java/edu/cuny/brooklyn/project/treasure/CircleTreasure.java
@@ -0,0 +1,40 @@
+package edu.cuny.brooklyn.project.treasure;
+
+import edu.cuny.brooklyn.project.GameSettings;
+
+public class CircleTreasure extends Treasure {
+	private int[][] treasure;
+	private int size;
+	
+	public CircleTreasure(int size, TreasureValueSetter setter) {
+		this.size = size;
+		treasure = new int[size][size];
+		
+		for (int i=0; i<treasure.length; i++) {
+			for (int j=0; j<treasure[i].length; j++) {
+				int cellValue = setter.getCellValue(i,j);
+				treasure[i][j] = cellValue;
+				setTotalValue(getTotalValue() + cellValue);
+			}
+		}		
+	}
+	
+	public CircleTreasure(int size) {//this needs to be fixed
+		this(size, (x,y) -> GameSettings.DEFAULT_TREASURE_VALUE);/*{if 
+		});*/
+	}
+
+	@Override
+	public int getValueAt(int x, int y) {
+		return treasure[y][x];
+	}
+
+	@Override
+	public int getBoundingBoxWidth() {
+		return size;
+	}
+
+	@Override
+	public int getBoundingBoxLength() {
+		return size;
+	}
